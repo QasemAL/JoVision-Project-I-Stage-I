834c6e9184777ca2a683c6e9bea0766abd3ceaa3
QasemAL <129743072+QasemAL@users.noreply.github.com>
2024-07-28 12:48:12 +0300

    GallaryScreen

--- JoVisionProject/Screens/Project.js
+++ JoVisionProject/Screens/Project.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CameraScreen from "./CameraS/Camera";
 import Screen2 from "./Screen2";
-import Screen3 from "./Screen3";
+import Gallary from "./Gallary";
 import Screen4 from "./Screen4";
 
 const Tab = createBottomTabNavigator();
@@ -16,7 +16,7 @@ const Project = () => {
         <Tab.Navigator>
           <Tab.Screen name="Screen1" component={CameraScreen} />
           <Tab.Screen name="Screen2" component={Screen2} />
-          <Tab.Screen name="Screen3" component={Screen3} />
+          <Tab.Screen name="Screen3" component={Gallary} />
           <Tab.Screen name="Screen4" component={Screen4} />
         </Tab.Navigator>
       </NavigationContainer>
