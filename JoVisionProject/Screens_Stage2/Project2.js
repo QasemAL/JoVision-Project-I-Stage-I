f90d4f713065cb3a2ff7dace81d98d116d9f276b
QasemAL <129743072+QasemAL@users.noreply.github.com>
2024-08-01 01:47:06 +0300

    Stage2_Done

--- JoVisionProject/Screens_Stage2/Project2.js
+++ JoVisionProject/Screens_Stage2/Project2.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CameraScreen from "./CameraS/Camera";
-import sensors from "./SensorS/sensors";
+import Sensors from "./SensorS/sensors";
 import Gallary from "./Gallary";
 import FullScreenView from "./Slideshow";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const MainTabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Screen1" component={CameraScreen} />
+    <Tab.Screen name="Screen2" component={Sensors} />
+    <Tab.Screen name="Screen3" component={Gallary} />
+  </Tab.Navigator>
+);
+
+const AppNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }} />
+    <Stack.Screen name="Screen4" component={FullScreenView} />
+  </Stack.Navigator>
+);
 
 const Project = () => {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Screen1" component={CameraScreen} />
-          <Tab.Screen name="Screen2" component={sensors} />
-          <Tab.Screen name="Screen3" component={Gallary} />
-          <Tab.Screen name="Screen4" component={FullScreenView}  />
-        </Tab.Navigator>
+        <AppNavigator />
       </NavigationContainer>
     </View>
   );
@@ -26,7 +38,8 @@ const Project = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,width:'100%',
+    flex: 1,
+    width: '100%',
   },
 });
 
